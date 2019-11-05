234395e2faf09537cd2288f954338623a720124f
ManuelM <descom666999@gmail.com>
2019-11-05 19:17:21 +0100

    Initial commit

---
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica5/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package net.iessochoa.manuelmartinez.practica5.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.iessochoa.manuelmartinez.practica5.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica5/modelo/DiarioContract.java
@@ -0,0 +1,16 @@
+package net.iessochoa.manuelmartinez.practica5.modelo;
+
+import android.provider.BaseColumns;
+
+public class DiarioContract {
+
+    public static class DiaDiarioEntries {
+        public static final String TABLE_NAME = "diadiario"; //nombre de la base de datos
+        public static final String ID = BaseColumns._ID;//esta columna es necesaria para Android
+        public static final String FECHA = "fecha";
+        public static final String FECHA = "fecha";
+        public static final String FECHA = "fecha";
+        public static final String FECHA = "fecha";
+    }
+
+}
