765d933463405ad2932d1309bb7acf93fa06f51b
descomkraneoz <descom666999@gmail.com>
2019-11-18 12:34:41 +0100

    trabajando en borrar

--- app/src/main/java/net/iessochoa/manuelmartinez/practica5/activities/MainActivity.java
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica5/activities/MainActivity.java
@@ -20,15 +20,18 @@ import net.iessochoa.manuelmartinez.practica5.modelo.DiaDiario;
 import net.iessochoa.manuelmartinez.practica5.modelo.DiarioContract;
 import net.iessochoa.manuelmartinez.practica5.modelo.DiarioDB;
 
-import java.util.Date;
-
 public class MainActivity extends AppCompatActivity {
+    //Constante para mandar datos de una actividad a otra cuando se edita o crea una nueva entrada al diario
     public final static int REQUEST_OPTION_NUEVA_ENTRADA_DIARIO = 0;
-    public static final int REQUEST_OPTION_EDITAR_ENTRADA_DIARIO = 1;
+    //proximamente usare estas dos ctes.
+    //public static final int REQUEST_OPTION_EDITAR_ENTRADA_DIARIO = 1;
     //public static String STATE_LISTA_DIAS = "net.iessochoa.manuelmartinez.practica5.activities.EdicionDiaActivity.lista_dias";
-
-    private DiarioDB db;
+    //Declaracion de los distintos elementos
     Button btAcercade;
+    //Variable que almacena la base de datos
+    private DiarioDB db;
+    //Variable para saber el orden actual en el que se mostraran los elementos en el tvPrincipal
+    private String ordenActualDias;
     Button btAnyadir;
     Button btOrdenar;
     Button btFecha;
@@ -36,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
     Button btResumen;
     Button btBorrar;
     TextView tvPrincipal;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,17 +54,26 @@ public class MainActivity extends AppCompatActivity {
         btResumen = findViewById(R.id.btResumen);
 
 
+        //BASE DE DATOS, inicializamos y cargamos datos de prueba
+        try {
+            //Inicializa la base de datos
+            db = new DiarioDB(this);
+            //abre la base de datos
+            db.open();
+            //cargamos unos datos de prueba en la base de datos
+            db.cargaDatosPrueba();
 
-        //BASE DE DATOS
-        db = new DiarioDB(this);
-        db.open();
-        cargarDatosDePrueba();
-        mostrarDias(DiarioContract.DiaDiarioEntries.FECHA);
-
-
+        } catch (android.database.sqlite.SQLiteException e) {
+            e.printStackTrace();
+        }
+        //Ponemos un orden para mostrar los datos por defecto
+        ordenActualDias = DiarioContract.DiaDiarioEntries.FECHA;
+        //Lo mostramos
+        mostrarDias();
 
     }
 
+    //Sobreescribimos el metodo onDestroy para que cierre la base de datos cuando se cierre la aplicacion
     @Override
     protected void onDestroy() {
         db.close();
@@ -71,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * @param menu, para poder mostrar el menu hace falta inflarlo antes.
+     * @param menu, para poder mostrar el menu de la aplicacion hace falta inflarlo antes tal que asi.
      */
 
     @Override
@@ -108,7 +118,9 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void agregaDiaDiario() {
+        //creamos un intent y le pasamos la actividad que llama a la actividad que recibe
         Intent intent = new Intent(MainActivity.this, EdicionDiaActivity.class);
+        //iniciamos el intent y el pasamos una constante para guardar/enviar los datos
         startActivityForResult(intent, REQUEST_OPTION_NUEVA_ENTRADA_DIARIO);
     }
 
@@ -120,16 +132,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.btAcercade:
+                //Muestra el dialogo Acerca de
                 MensajeAcercade();
                 break;
             case (R.id.btAnyadir):
+                //Llama al metodo para agregar un nuevo dia en EdicionDiaActivity
                 agregaDiaDiario();
                 break;
             case R.id.btOrdenar:
+                //Llama al metodo para ordenar según unos parámetros
                 menuOrdenarPor();
                 break;
             case R.id.btBorrar:
-                borrarDias();
+                //Llama al metodo para borrar el primer dia
+                borrarPrimerDia();
                 break;
             case R.id.btValorarVida:
                 Toast.makeText(getApplicationContext(), getResources().getText(R.string.tmMensajeERROR), Toast.LENGTH_LONG).show();
@@ -145,13 +161,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Metodo para mostrar
+     * Metodo para mostrar en el textview del MainActivity los datos de la base de datos
+     */
+
+    private void mostrarDias() {
+        //Obtenemos un cursor el cual esta ordenado por el orden que hay por defecto
+        Cursor c = db.obtenDiario(ordenActualDias);
+        //Almacenamos el dia
+        DiaDiario dia;
+        //limpiamos el campo de texto
+        tvPrincipal.setText("");
+        //Nos aseguramos de que existe al menos un registro
+        if (c.moveToFirst()) {
+            //Recorremos el cursor hasta que no haya más registros
+            do {
+                dia = DiarioDB.deCursorADia(c);
+                //podéis sobrecargar toString en DiaDiario para mostrar los datos
+                //tvPrincipal.append(dia.toString() + "\n");
+                tvPrincipal.append(dia.mostrarDatosBonitos() + "\n");
+            } while (c.moveToNext());
+        }
+    }
+
+    /**
+     * Metodo para mostrar en el textview del MainActivity los datos de la base de datos
+     * en funcion a un orden que me pasan
      */
 
     private void mostrarDias(String ordenadoPor) {
+        //Obtenemos un cursor el cual esta ordenado por el orden que me pasan
         Cursor c = db.obtenDiario(ordenadoPor);
+        //Almacenamos el dia
         DiaDiario dia;
-        tvPrincipal.setText("");//limpiamos el campo de texto
+        //limpiamos el campo de texto
+        tvPrincipal.setText("");
         //Nos aseguramos de que existe al menos un registro
         if (c.moveToFirst()) {
             //Recorremos el cursor hasta que no haya más registros
@@ -168,8 +211,14 @@ public class MainActivity extends AppCompatActivity {
      * Metodo para borrar dias del diario
      */
 
-    private void borrarDias() {
-        db.borraDia();
+    private void borrarPrimerDia() {
+        //Obtenemos un cursor el cual esta ordenado por el orden que hay actualmente
+        Cursor c = db.obtenDiario(ordenActualDias);
+        //Nos intentamos mover a la primera posición del cursor
+        if (c.moveToFirst()) {
+            //Eliminamos la tupla de la base de datos obteniendo la información del cursor
+            db.borraDia(DiarioDB.deCursorADia(c));
+        }
         Toast.makeText(getApplicationContext(), getResources().getText(R.string.tmMensajeBorrar), Toast.LENGTH_LONG).show();
         mostrarDias(DiarioContract.DiaDiarioEntries.FECHA);
     }
@@ -189,13 +238,12 @@ public class MainActivity extends AppCompatActivity {
                         mostrarDias(DiarioContract.DiaDiarioEntries.CONTENIDO);
                         break;
                 }
-
             }
         };
 
     }
 
-    private void cargarDatosDePrueba() {
+    /*private void cargarDatosDePrueba() {
         DiaDiario d = new DiaDiario(new Date("02/11/2002"),
                 5, "Examen de Lenguaje de Marcas",
                 "Los temas que entran son HTML y CSS, deberas hacer una página" +
@@ -208,18 +256,27 @@ public class MainActivity extends AppCompatActivity {
                         "lo pasaremos bien");
         db.insertaDia(d);
         db.insertaDia(i);
-    }
+    }*/
+
+    /**
+     * Método que se ejecuta cuando se vuelve de una actividad iniciada con startActivityForResult
+     */
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //Miramos que el resultado devuelto sea correcto y que el código de solicitud sea el de añadir una nueva actividad
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case REQUEST_OPTION_NUEVA_ENTRADA_DIARIO:
+                    //recuperamos los datos de la otra actividad y los guardamos en un objeto DiaDiario
                     DiaDiario p = data.getParcelableExtra(EdicionDiaActivity.EXTRA_DIA_A_GUARDAR);
+                    //Guardamos en la base de datos el objeto recuperado
                     db.insertaDia(p);
-                    mostrarDias(DiarioContract.DiaDiarioEntries.FECHA);
+                    //Usamos el metodo mostrarDias para enseñar la base de datos con todos los datos
+                    mostrarDias();
                     break;
+                //De momento esta parte no la usamos
                 /*case REQUEST_OPTION_EDITAR_POBLACIONES:
                     Poblacion pi = data.getParcelableExtra(PoblacionActivity.EXTRA_POBLACION_A_GUARDAR);
                     editarPoblacion(pi);
--- app/src/main/java/net/iessochoa/manuelmartinez/practica5/modelo/DiarioDB.java
+++ app/src/main/java/net/iessochoa/manuelmartinez/practica5/modelo/DiarioDB.java
@@ -10,7 +10,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import net.iessochoa.manuelmartinez.practica5.modelo.DiarioContract.DiaDiarioEntries;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -37,12 +36,16 @@ public class DiarioDB {
     private final static String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + DiaDiarioEntries.TABLE_NAME;
 
 
-    //DBHelper
+    /**
+     * DBHelper
+     */
 
-    //Nos permitira abrir la base de datos
+
+    //variables necesarias para la base de datos
     private DiaDiarioDbHelper dbH;
     private SQLiteDatabase db;
 
+    //Nos permitira crear la base de datos
     public DiarioDB(Context context) {
         dbH = new DiaDiarioDbHelper(context);
     }
@@ -54,9 +57,21 @@ public class DiarioDB {
         }
     }
 
-    public void close() throws SQLiteException {
-        if (db.isOpen())
-            db.close();
+    /**
+     * Metodos para la fecha
+     */
+
+    public static Date fechaBDtoFecha(String f) {
+        //Varibale tipo date para almacenar la fecha
+        Date fecha = null;
+        try {
+            //Se pasa el string a Date aplicandole el formato indicado en el SimpleDateFormat y se guarda en la variable
+            fecha = new SimpleDateFormat("dd/MM/yyyy").parse(f);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        //Se devuelve la fecha
+        return fecha;
     }
 
     //Clase interna con la que decir a android que cree la BD y lo que hacer cuando se modifica la version de la BD
@@ -81,29 +96,33 @@ public class DiarioDB {
         }
     }
 
-    /**
-     * Metodos para la fecha
-     */
-
-    public static Date fechaBDtoFecha(String f) {
-        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("dd/MM/yyyy");
-        Date fecha = null;
-        try {
-            fecha = formatoDelTexto.parse(f);
-
-        } catch (ParseException ex) {
-
-            ex.printStackTrace();
-
-        }
-        return fecha;
-    }
-
     public static String fechaToFechaDB(Date fecha) {
-        DateFormat f = new SimpleDateFormat("MM/dd/yyyy");
-        return f.format(fecha);
+        //Se pasa la fecha a String aplicandole el formato del SimpleDateFormat y se devuelve
+        return new SimpleDateFormat("dd/MM/yyyy").format(fecha);
     }
 
+    /**
+     * Dada una posición del cursor, nos devuelve un objeto Dia
+     */
+    public static DiaDiario deCursorADia(Cursor c) {
+        //Obtenemos los datos del cursor por medio del nombre de la columna dado en DiaDiarioEntry
+        Date fecha = DiarioDB.fechaBDtoFecha(c.getString(c.getColumnIndex(DiaDiarioEntries.FECHA)));
+        int valoracionDia = c.getInt((c.getColumnIndex(DiaDiarioEntries.VALORACION)));
+        String resumen = c.getString(c.getColumnIndex(DiaDiarioEntries.RESUMEN));
+        String contenido = c.getString(c.getColumnIndex(DiaDiarioEntries.CONTENIDO));
+        String fotoUri = c.getString(c.getColumnIndex(DiaDiarioEntries.FOTO_URI));
+        String latitud = c.getString(c.getColumnIndex(DiaDiarioEntries.LATITUD));
+        String longitud = c.getString(c.getColumnIndex(DiaDiarioEntries.LONGITUD));
+        //Devolvemos un nuevo DiaDiario con los datos obtenidos
+        return new DiaDiario(fecha, valoracionDia, resumen, contenido, fotoUri, latitud, longitud);
+    }
+
+    //Cierra la base de datos
+    public void close() throws SQLiteException {
+        if (db.isOpen()) {
+            db.close();
+        }
+    }
 
     /**
      * CRUD del sql
@@ -112,41 +131,51 @@ public class DiarioDB {
     //Inserta nuevo Dia en el diario pasado por parametro o lo actualiza si ya existe
     public void insertaDia(DiaDiario dia) throws SQLiteException, SQLiteConstraintException {
         ContentValues values = new ContentValues();
-        if (!dia.getFecha().equals("")) {
-            values.put(DiaDiarioEntries.FECHA, fechaToFechaDB(dia.getFecha()));
-        }
+        //Introducimos los valores del nuevo día. Para ello se indica en cada put el nombre
+        // de la columna,dado por la clase DiaDiarioEntry y el valor que tenga
+        values.put(DiaDiarioEntries.FECHA, DiarioDB.fechaToFechaDB(dia.getFecha()));
         values.put(DiaDiarioEntries.VALORACION, dia.getValoracionResumida());
         values.put(DiaDiarioEntries.RESUMEN, dia.getResumen());
         values.put(DiaDiarioEntries.CONTENIDO, dia.getContenido());
-        values.put(DiaDiarioEntries.FOTO_URI, dia.getFotoUri());
-        values.put(DiaDiarioEntries.LATITUD, dia.getLatitud());
-        values.put(DiaDiarioEntries.LONGITUD, dia.getLongitud());
+
+        //Miramos si estos valores estan vacios,ya que son opcionales y pueden estarlo
+        //En caso de no estar vacios los añadimos,en caso contrario no
+        if (dia.getFotoUri() != null && !dia.getFotoUri().isEmpty()) {
+            values.put(DiaDiarioEntries.FOTO_URI, dia.getFotoUri());
+        }
+        if (dia.getLatitud() != null && !dia.getLatitud().isEmpty()) {
+            values.put(DiaDiarioEntries.LATITUD, dia.getLatitud());
+        }
+        if (dia.getLongitud() != null && !dia.getLongitud().isEmpty()) {
+            values.put(DiaDiarioEntries.LONGITUD, dia.getLongitud());
+        }
         //Si queremos que salte la excepción en caso de problemas en la insercción
         //tenemos que utilizar insertOrThrow, en otro caso podemos utilizar insert
         try {
             db.insertOrThrow(DiaDiarioEntries.TABLE_NAME, null, values);
         } catch (SQLiteException sql) {
+            //restriccion para la instruccion sql update
             String where = DiaDiarioEntries.FECHA + "=?";
-            String[] arg = new String[]{fechaToFechaDB(dia.getFecha())};
+            ////Obtenemos el valor de la fecha de la tupla que queremos insertar/modificar
+            String[] arg = new String[]{DiarioDB.fechaToFechaDB(dia.getFecha())};
             //actualizamos si ya existe en la base de datos
             db.update(DiaDiarioEntries.TABLE_NAME, values, where, arg);
         }
-
     }
 
-
-    //Borra el dia
-    public void borraDia() throws SQLiteException, SQLiteConstraintException {
-        Cursor cursor = db.query(DiaDiarioEntries.TABLE_NAME, null, null, null,
-                null, null, DiaDiarioEntries.FECHA);
-        cursor.moveToFirst();
-        System.out.println(db.delete(DiaDiarioEntries.TABLE_NAME, DiaDiarioEntries.ID + "=?", null));
-
-        //System.out.println(DiarioDB.deCursorADia(cursor).mostrarDatosBonitos());
-
+    //Borra una tupla de la base de datos
+    public void borraDia(DiaDiario dia) throws SQLiteException, SQLiteConstraintException {
+        //Obtenemos el valor de la fecha de la tupla que queremos borrar
+        String[] args = {DiarioDB.fechaToFechaDB(dia.getFecha())};
+        //Ponemos la restriccion para la instruccion sql
+        String where = DiaDiarioEntries.FECHA + " = ?";
+        //Ejecutamos el método para la eliminacion de la tupla
+        db.delete(DiaDiarioEntries.TABLE_NAME, where, args);
     }
 
-    //Cursor para mostrar un dia
+    /**
+     * Método que devuelve un cursor con todas las tuplas de la base de datos ordenadas por el parametro pasado
+     */
     public Cursor obtenDiario(String ordenadoPor) throws SQLiteException {
 
         return db.query(DiaDiarioEntries.TABLE_NAME,
@@ -159,32 +188,23 @@ public class DiarioDB {
     }
 
     /**
-     * Dada una posición del cursor, nos devuelve un objeto Dia
+     * Metodo que inserta en la base de datos los siguientes datos
      */
-    public static DiaDiario deCursorADia(Cursor cursor) {
-        int indiceColumna;
-        //obtenemos la posicion de la columna id
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.ID);
-        //obtenemos el valor del id
-        String id = cursor.getString(indiceColumna);
-        //obtenemos el resto de posiciones de las columnas
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.FECHA);
-        Date fecha = fechaBDtoFecha(cursor.getString(indiceColumna));
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.VALORACION);
-        int valoracion = cursor.getInt(indiceColumna);
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.RESUMEN);
-        String resumen = cursor.getString(indiceColumna);
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.CONTENIDO);
-        String contenido = cursor.getString(indiceColumna);
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.FOTO_URI);
-        String foto = cursor.getString(indiceColumna);
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.LATITUD);
-        String latitud = cursor.getString(indiceColumna);
-        indiceColumna = cursor.getColumnIndex(DiaDiarioEntries.LONGITUD);
-        String longitud = cursor.getString(indiceColumna);
-
-        return new DiaDiario(fecha, valoracion, resumen, contenido, foto, latitud, longitud);
-
+    public void cargaDatosPrueba() {
+        DiaDiario[] dias = {new DiaDiario(new Date("02/11/2002"),
+                5, "Examen de Lenguaje de Marcas",
+                "Los temas que entran son HTML y CSS, deberas hacer una página" +
+                        " web con la estructura típica, y contestar veinte preguntas de " +
+                        "tipo test en 30 minutos"),
+                new DiaDiario(new Date("28/03/2018"),
+                        10, "Cumpleaños de Manu",
+                        "Fiesta de cumpleaños en Chikipark" +
+                                " traer tortada del Zipi-Zape y comprar regalos en Amazon " +
+                                "lo pasaremos bien")};
+        for (DiaDiario d : dias) {
+            this.insertaDia(d);
+        }
     }
+
 
 }
